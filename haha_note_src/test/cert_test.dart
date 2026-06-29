069aab6d1762dd5ee4268cab7b3b1e4d3b938287
Bandeapart1964 <Bandeapart1964@catpuppyapp.com>
2026-06-29 17:46:19 +0800

    update tls cert

--- haha_note_src/test/cert_test.dart
+++ haha_note_src/test/cert_test.dart
@@ -12,7 +12,8 @@ import 'package:cloud_disk_note_app/cloud_disk_note/storage/utils.dart';
 /// 5. 启动程序，测试网络连接是否正常
 Future<void> main() async {
   final prefixString = r'''
-// 更新于：20260320
+// 更新于：20260629
+// 证书包发布日期：20260514
 // 来源：https://curl.se/docs/caextract.html
 // r代表raw字符串，可保留转义字符，例如\、\n等
 // """这种格式代表多行字符串，可保留换行符"""
