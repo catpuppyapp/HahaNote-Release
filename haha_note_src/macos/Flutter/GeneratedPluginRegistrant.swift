d94c6110ef5ac524f28b1cc514e856ea92c48f4d
Bandeapart1964 <luckyclover33xx@gmail.com>
2026-07-06 13:23:29 +0800

    upgrade flutter to 3.44.4

--- haha_note_src/macos/Flutter/GeneratedPluginRegistrant.swift
+++ haha_note_src/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,7 +9,6 @@ import cryptography_flutter
 import device_info_plus
 import file_selector_macos
 import package_info_plus
-import path_provider_foundation
 import screen_retriever_macos
 import url_launcher_macos
 import video_player_avfoundation
@@ -22,7 +21,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   VideoPlayerPlugin.register(with: registry.registrar(forPlugin: "VideoPlayerPlugin"))
