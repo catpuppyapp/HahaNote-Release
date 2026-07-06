a8c8233e176019619bc967dc4bbaf75724ff711e
Bandeapart1964 <luckyclover33xx@gmail.com>
2026-07-07 07:24:19 +0800

    add rust-toolchain.toml, disable github rust action

--- haha_note_src/build_script/prebuild.sh
+++ haha_note_src/build_script/prebuild.sh
@@ -1,3 +1,12 @@
+# install rust toolchain
+# install rustup
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# run the command will trigger rust toolchains download, 
+# the version specified rust-toolchain.toml file, 
+# find current dir first, if not found, find parent, most maybe 3 layers dirs
+rustc -Vv
+
+
 # flutter pub get then use sed to add build-id=none to CMakeLists.txt for pass the RB, else maybe generate difference apks
 # see: https://codeberg.org/IzzyOnDroid/repodata/issues/327#issuecomment-18667955
 flutter pub get
@@ -6,8 +15,5 @@ echo ${PUB_CACHE}
 
 echo rust print: rustc version is:
 rustc -Vv
-
-echo rust print: rustc env RUSTUP_TOOLCHAIN is:
-echo ${RUSTUP_TOOLCHAIN}
 
 sed -i -e 's/-Wl,/-Wl,--build-id=none,/' ${PUB_CACHE}/hosted/*/jni-*/src/CMakeLists.txt
