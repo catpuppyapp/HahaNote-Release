61db0fb14fd71af9343c35d44c6dc39f1106a0b0
Bandeapart1964 <luckyclover33xx@gmail.com>
2026-07-07 06:14:57 +0800

    print rustc versoin when build apk from workflow

--- haha_note_src/build_script/prebuild.sh
+++ haha_note_src/build_script/prebuild.sh
@@ -1,6 +1,13 @@
 # flutter pub get then use sed to add build-id=none to CMakeLists.txt for pass the RB, else maybe generate difference apks
 # see: https://codeberg.org/IzzyOnDroid/repodata/issues/327#issuecomment-18667955
 flutter pub get
-echo flutter_pub_cache_path_is:
+echo flutter print: flutter_pub_cache_path_is:
 echo ${PUB_CACHE}
+
+echo rust print: rustc version is:
+rustc -Vv
+
+echo rust print: rustc env RUSTUP_TOOLCHAIN is:
+echo ${RUSTUP_TOOLCHAIN}
+
 sed -i -e 's/-Wl,/-Wl,--build-id=none,/' ${PUB_CACHE}/hosted/*/jni-*/src/CMakeLists.txt
