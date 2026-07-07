fe76b6d35095dcfde83987f0a00bb61b40129669
Bandeapart1964 <luckyclover33xx@gmail.com>
2026-07-07 11:59:59 +0800

    update script

--- haha_note_src/build_script/prebuild.sh
+++ haha_note_src/build_script/prebuild.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+pushd ..
 # flutter pub get then use sed to add build-id=none to CMakeLists.txt for pass the RB, else maybe generate difference apks
 # see: https://codeberg.org/IzzyOnDroid/repodata/issues/327#issuecomment-18667955
 flutter pub get
@@ -13,3 +14,4 @@ echo "will try insert 'build-id=none' to 'flutter pub cache/middle dirs/CMakeLis
 # find then sed to avoid throw err, it will only run sed when file exists
 find "${PUB_CACHE}/hosted/" -path "*/jni-*/src/CMakeLists.txt" -exec sed -i -e 's/-Wl,/-Wl,--build-id=none,/' {} +
 
+popd
