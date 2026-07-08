f0fd20fb9036d893372793b7a1aa0d19327f0be7
Bandeapart1964 <luckyclover33xx@gmail.com>
2026-07-08 13:28:48 +0800

    fix cargokit apply.sh

--- haha_note_src/build_script/cargokit_patch/apply.sh
+++ haha_note_src/build_script/cargokit_patch/apply.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-
-cp -f cargokit.yaml "${PUB_CACHE}/hosted/pub.dev/code_forge-*/rust/cargokit.yaml"
-cp -f options.dart "${PUB_CACHE}/hosted/pub.dev/code_forge-*/cargokit/build_tool/lib/src/options.dart"
+# path cannot add quote sign "", else the path will not expand
+CODE_FORGE_BASE=$(ls -d ${PUB_CACHE}/hosted/pub.dev/code_forge-*)
+cp -f cargokit.yaml "${CODE_FORGE_BASE}/rust/cargokit.yaml)"
+cp -f options.dart "${CODE_FORGE_BASE}/cargokit/build_tool/lib/src/options.dart"
