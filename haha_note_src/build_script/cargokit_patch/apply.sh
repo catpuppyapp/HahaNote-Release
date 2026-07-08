ff74b788d1c728d7ce38e23d622f5b3cdcefce3f
Bandeapart1964 <luckyclover33xx@gmail.com>
2026-07-08 14:29:58 +0800

    update cargokit patch apply.sh: comment test cmd

--- haha_note_src/build_script/cargokit_patch/apply.sh
+++ haha_note_src/build_script/cargokit_patch/apply.sh
@@ -10,7 +10,7 @@ CODE_FORGE_BASE=$(ls -d ${PUB_CACHE}/hosted/pub.dev/code_forge-*)
 cp -f cargokit.yaml "${CODE_FORGE_BASE}/rust/cargokit.yaml"
 cp -f options.dart "${CODE_FORGE_BASE}/cargokit/build_tool/lib/src/options.dart"
 
-# test set -e (expect: after commands will not execute)
-cp -f abc.txt /bad/path/abc/def/gkdjgkdjglkd.txt
+# test set -e (expect: after commands will not execute, result: passed)
+# cp -f abc.txt /bad/path/abc/def/bad.txt.1701428416127150
 
 echo "END: apply cargokit_patch for code_forge"
