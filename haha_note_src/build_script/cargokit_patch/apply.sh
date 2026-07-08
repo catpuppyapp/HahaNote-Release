8786c7dcbe2a173f22c2110567ab95a3c2b034a9
Bandeapart1964 <luckyclover33xx@gmail.com>
2026-07-08 12:50:39 +0800

    add cargokit_patch to resolve rustc version can not specified

---
+++ haha_note_src/build_script/cargokit_patch/apply.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+
+cp -f cargokit.yaml "${PUB_CACHE}/hosted/pub.dev/code_forge-*/rust/cargokit.yaml"
+cp -f options.dart "${PUB_CACHE}/hosted/pub.dev/code_forge-*/cargokit/build_tool/lib/src/options.dart"
