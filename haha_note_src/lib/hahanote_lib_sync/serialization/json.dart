3bc4dd79f2de12088efaf36af137e4ba5942adb9
Bandeapart1964 <luckyclover33xx@gmail.com>
2026-07-11 09:28:54 +0800

    add comments for json serializable

--- haha_note_src/lib/hahanote_lib_sync/serialization/json.dart
+++ haha_note_src/lib/hahanote_lib_sync/serialization/json.dart
@@ -1,5 +1,8 @@
 import 'package:json_annotation/json_annotation.dart';
 
-/// 若不加 explicitToJson: true，则 instance.fromJson(instance.toJson)
-/// 有可能因为嵌套非直接能转换成json的类型而失败，例如TimeData类，就会转换失败
+/// `explicitToJson: true` 的作用: 
+/// 生成的toJson函数会显式调用嵌套字段的toJson，避免转换失败。
+/// 若设为false则在实例包含其他对象时，即使对象实现了toJson函数，
+/// 调用 instance.fromJson(instance.toJson) 也会报错，
+/// 因为生成的代码里不会显式调用对象的toJson
 const myJsonSerializable = JsonSerializable(explicitToJson: true);
