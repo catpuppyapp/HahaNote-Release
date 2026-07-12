92e8322a494b3bbffd8a5754067a7eb5bebbc98d
Bandeapart1964 <luckyclover33xx@gmail.com>
2026-07-12 09:12:55 +0800

    add `MyHtmlVideoAudioExtension`

---
+++ haha_note_src/lib/widget/media_bar.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:hahanote_app/util/reveal_file.dart';
+import 'package:hahanote_app/util/util.dart';
+import 'package:path/path.dart' as p;
+
+import '../hahanote_lib_sync/utils.dart';
+
+const _TAG = "MediaBar";
+
+class MediaBar extends StatefulWidget {
+  final String path;
+  final IconData headingIcon;
+  final void Function(String) showMsg;
+  final void Function(String) showMsgLong;
+
+  const MediaBar({
+    super.key,
+    required this.path,
+    required this.headingIcon,
+    required this.showMsg,
+    required this.showMsgLong,
+  });
+
+  @override
+  State<MediaBar> createState() => _MediaBarState();
+
+}
+
+class _MediaBarState extends State<MediaBar> {
+  bool isRelativePath = false;
+  String fileName = "";
+
+  @override
+  void initState() {
+    super.initState();
+    isRelativePath = !isHttpUrl(widget.path);
+    fileName = p.basename(widget.path);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(widget.headingIcon),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(fileName),
+            const Divider(),
+            Row(
+              children: [
+                IconButton(
+                  onPressed: () {
+                    if(isRelativePath) {
+                      openFileInExternal(widget.path, showMsgLong: widget.showMsgLong, callerTag: _TAG);
+                    }else {
+                      launchUrlExtByStr(widget.path);
+                    }
+                  },
+                  icon: Icon(Icons.play_circle_outline),
+                ),
+                IconButton(
+                  onPressed: () {
+                    copyText(widget.path);
+                  },
+                  icon: Icon(Icons.copy),
+                ),
+                if(isRelativePath) IconButton(
+                  onPressed: () {
+                    revealFile(widget.path, showMsgLong: widget.showMsgLong);
+                  },
+                  icon: Icon(Icons.folder_outlined),
+                ),
+              ],
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
---
+++ haha_note_src/lib/widget/my_html_video_audio_extension.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:hahanote_app/widget/media_bar.dart';
+
+/// [MyHtmlVideoAudioExtension] adds support for the <video> and <audio> tag to the flutter_html
+/// library.
+class MyHtmlVideoAudioExtension extends HtmlExtension {
+  final bool isVideo;
+  final String basePath;
+  final void Function(String) showMsg;
+  final void Function(String) showMsgLong;
+
+  const MyHtmlVideoAudioExtension({
+    required this.isVideo,
+    required this.basePath,
+    required this.showMsg,
+    required this.showMsgLong,
+  });
+
+  @override
+  Set<String> get supportedTags => {"audio"};
+
+  @override
+  InlineSpan build(ExtensionContext context) {
+    return WidgetSpan(
+      child: AudioWidget(
+        context: context,
+        isVideo: isVideo,
+        basePath: basePath,
+        showMsg: showMsg,
+        showMsgLong: showMsgLong,
+      ));
+  }
+}
+
+/// A widget used for rendering an audio player in the HTML tree
+class AudioWidget extends StatefulWidget {
+  final ExtensionContext context;
+  final bool isVideo;
+  final String basePath;
+  final void Function(String) showMsg;
+  final void Function(String) showMsgLong;
+
+  const AudioWidget({
+    super.key,
+    required this.context,
+    required this.isVideo,
+    required this.basePath,
+    required this.showMsg,
+    required this.showMsgLong,
+  });
+
+  @override
+  State<StatefulWidget> createState() => _AudioWidgetState();
+}
+
+class _AudioWidgetState extends State<AudioWidget> {
+  late final List<String?> sources;
+
+  @override
+  void initState() {
+    super.initState();
+    sources = <String?>[
+      if (widget.context.attributes['src'] != null)
+        widget.context.attributes['src'],
+      ...ReplacedElement.parseMediaSources(widget.context.node.children),
+    ];
+
+  }
+
+  @override
+  Widget build(BuildContext bContext) {
+    if (sources.isEmpty) {
+      return const SizedBox.shrink();
+    }
+
+    return CssBoxWidget(
+      style: widget.context.styledElement!.style,
+      childIsReplaced: true,
+      child: ListView.builder(
+        itemCount: sources.length,
+        itemBuilder: (context, index) {
+          final item = sources[index];
+          if(item == null) {
+            return const SizedBox.shrink();
+          }
+
+          return MediaBar(
+            path: item,
+            headingIcon: widget.isVideo ? Icons.movie_creation_outlined : Icons.music_note_outlined,
+            showMsg: widget.showMsg,
+            showMsgLong: widget.showMsgLong
+          );
+        }
+      ),
+    );
+  }
+}
